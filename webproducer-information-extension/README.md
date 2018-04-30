8a23f16ea9921f3afd839509dfba8b54fad8edf4
Frederik Dussault <fdussault7@gmail.com>
2018-04-30 10:58:00 -0400

    repair git history of wordpress-information-tab

--- webproducer-information-extension/README.md
+++ webproducer-information-extension/README.md
@@ -1,29 +1,18 @@
-# WebProducer information from active tab extension
+# WebProducer information extension
+
+## Concept
 
 ## Current state of the project
 
-*Version* 1.0  
+*Version 1.0* - 
 
-## Installation
+## Development environment
 
-In Chrome: 
+The project is based on node.js / grunt.
 
-1) open a new tab (or window) and type *chrome://extensions*
+The dependencies are:
+    grunt grunt-cli load-grunt-tasks grunt-contrib-jshint grunt-contrib-watch grunt-sass grunt-contrib-concat express
 
-1) Drag the *webproducer-information-extension.crx* file and drop it over the Chrome window.
+QUnit is used to test the application.
 
-1) Make sure the extension is activated
-
-## How to use it
-
-First, open the Chrome DevTools (F12)
-
-Click on the *WebProducer Information* icon. A popup appears under or near the button.
-
-Select an element from the drop-down
-
-## Improvements and update requests
-
-Version 1.0 features items needed for my work with Texture site.
-
-Feel free to ask for updates.
+## Production Environment
--- webproducer-information-extension/src/popup.js
+++ webproducer-information-extension/src/popup.js
@@ -31,161 +31,17 @@ function getCurrentTabUrl(callback) {
 // current.
 document.addEventListener('DOMContentLoaded', () => {
 
-  var wpClasses = [];
-
-  getCurrentTabUrl( (tab) => {  // this closure is the callback
-  
-    /**
-     * @description trim and remove blanks around comma separator
-     * @param {string} toSanitize
-     * @returns {string}
-     * note: trim spaces around commas and remove extra commas
-     */
-    function sanitizeClassList(toSanitize) {
-        return toSanitize
-                .split(',')
-                .reduce( (arr, s) => {
-                    s = s.trim();
-                    if (s) arr.push(s);
-                    return arr;
-                  }, [])
-                .join(',');
-    }
-
-    // get stored values
-    let prefixClassInput = document.getElementById('wpclass');
-    chrome.storage.sync.get(['classPrefixes'], function(result) {
-        prefixClassInput.value = result.classPrefixes;
-        console.log('result.classPrefixes ' + result.classPrefixes);
-        console.log('prefixClassInput.value: ' + prefixClassInput.value);
-    });
-    prefixClassInput.addEventListener('change', function() {
-        //sanitize
-        prefixClassInput.value = sanitizeClassList(prefixClassInput.value);
-        console.log('prefixClassInput.value: ' + prefixClassInput.value);
-
-        wpClasses = (prefixClassInput.value).split(',');
-        console.log('wpClasses ' + wpClasses);
-
-        debugger;
-        dropdown.dispatchEvent(new Event('change', { 'bubbles': true }));
-    }); // end prefixClassInput addEventListener('change')
-    
-    // populate the task dropdown
-    let dropdown = document.getElementById('dropdown');
-    buildDropdown(dropdown);
-    dropdown.addEventListener('change', function() {
-
-      chrome.tabs.executeScript({ // this execute selected code on tab
-        code: `{ 
-            debugger;
-            console.log('wpClasses ' + wpClasses);
-              
-            ${buildScript(wpClasses)}
-
-            rdm.${ this.value }();
-        }`
-      });
-
-    }); // end dropdown addEventListener('change')
-
-  }); // end callback
-}); // end document addEventListener DOMContentLoaded
-
-
-/**
- * @description data used to pop-up drop-down items and associated command to execute
- */
-const data = [
-  {
-      'name': 'list',
-      'label': 'All the options',
-      'code': function() {
-          this.listHtmlClass();
-          this.listBodyClass();
-          this.listMetas();
-          this.listEditUrl();			
-      }
-  },
-  {
-      'name': 'listBodyClass',
-      'label': 'List classes applied on BODY tag',
-      'code': function() {
-          let el = document.getElementsByTagName('body')[0];
-          let values = el.className.split(' ');
-          let valuesJoined = values.join(''); 
-
-          console.log( 'RDM - List of classes on body tag' );
-          if ( valuesJoined ) {
-              console.table( values );
-          } else {
-              console.log( '    No classes found' );
-          }
-      }
-  },
-  {
-      'name': 'listHtmlClass',
-      'label': 'List classes applied on HTML tag',
-      'code': function() {
-          let el = document.getElementsByTagName('html')[0];
-          let values = el.className.split(' ');
-          let valuesJoined = values.join(''); 
-  
-          console.log( 'RDM - List of classes on html tag' );
-          if ( valuesJoined ) {
-          console.table( values );
-          } else {
-          console.log( '    No classes found' );
-          }
-      },
-  },
-  {
-      'name': 'listMetas',
-      'label': 'List the meta information',
-      'code': function() {
-          let elems = document.getElementsByTagName('meta'); 
-  
-          console.log( 'RDM - List of  metas' );
-          if ( elems && elems.length > 0 ) {
-          console.table( 
-              elems, 
-              ["name", "property", "http-equiv", "content", "container" ] 
-          );
-          } else {
-          console.log( '    No meta found' );
-          }
-      },
-  },
-  {
-      'name': 'listEditUrl',
-      'label': 'Show Wordpress edit page url',
-      'code': function() {
-          let el = document.getElementsByTagName('body')[0];
-          let elClasses = el.className.split(' ').filter(item => item !== "");
-          let found = false;
-          // let wpClasses = [ "page-id-", "postid-" ]; // Passed in buildScript()
-      
-          console.log( "RDM - Wordpress edit page url: ");
-          if ( elClasses.length > 0 ) {
-            // list them
-            for ( let cls of elClasses ) {
-              // display Wordpress edit url
-              found += logWPEditLink ( cls, wpClasses );
-            }
-          }
-          
-          if ( !found ) {
-            console.log( '    No Wordpress classes found' );
-          }
-      },
+  /**
+   * @description format a code as js litteral notation: `${name}: ${code.toString()}`
+   * @param {string} name function name
+   * @param {string} code function code
+   * @returns {string} formated code
+   */
+  function formatCode ( name, code, sep = ',\n\n' ) {
+    return `${name}: ${code.toString()}${sep}`
   }
-]; // end data
-
-/**
- * @description Build pop-up dropdown
- */
-function buildDropdown (dropdown) {
-
+  
+  
   /**
    * @description build html select field options based on tasks object
    * @param {string} elementID
@@ -200,83 +56,158 @@ function buildDropdown (dropdown) {
         optionEl.innerHTML = label;
     elemID.appendChild(optionEl);
   }
+  
+
+  let tasks = [
+    {
+        'name': 'list',
+        'label': 'All the options',
+        'code': function() {
+            this.listHtmlClass();
+            this.listBodyClass();
+            this.listMetas();
+            this.listEditUrl();			
+        }
+    },
+    {
+        'name': 'listBodyClass',
+        'label': 'List classes applied on BODY tag',
+        'code': function() {
+            let el = document.getElementsByTagName('body')[0];
+            let values = el.className.split(' ');
+            let valuesJoined = values.join(''); 
+
+            console.log( 'RDM - List of classes on body tag' );
+            if ( valuesJoined ) {
+                console.table( values );
+            } else {
+                console.log( '    No classes found' );
+            }
+        }
+    },
+    {
+        'name': 'listHtmlClass',
+        'label': 'List classes applied on HTML tag',
+        'code': function() {
+            let el = document.getElementsByTagName('html')[0];
+            let values = el.className.split(' ');
+            let valuesJoined = values.join(''); 
     
-  data.forEach( ({name, label, code}) => { 
-      appendToDropdown ( dropdown, name, label );
+            console.log( 'RDM - List of classes on html tag' );
+            if ( valuesJoined ) {
+            console.table( values );
+            } else {
+            console.log( '    No classes found' );
+            }
+        },
+    },
+    {
+        'name': 'listMetas',
+        'label': 'List the meta information',
+        'code': function() {
+            let elems = document.getElementsByTagName('meta'); 
+    
+            console.log( 'RDM - List of  metas' );
+            if ( elems && elems.length > 0 ) {
+            console.table( 
+                elems, 
+                ["name", "property", "http-equiv", "content", "container" ] 
+            );
+            } else {
+            console.log( '    No meta found' );
+            }
+        },
+    },
+    {
+        'name': 'listEditUrl',
+        'label': 'Show Wordpress edit page url',
+        'code': function() {
+            let el = document.getElementsByTagName('body')[0];
+        
+            // list them
+            for ( let cls of el.className.split(' ') ) {
+            // display Wordpress edit url
+            logWPEditLink ( cls, "page-id-" );
+            logWPEditLink ( cls, "postid-" );      
+            }
+        },
+    }
+  ]; // end tasks
+
+  let dropdown = document.getElementById('dropdown');
+  tasks.forEach( ({name, label, code}) => { 
+    appendToDropdown ( dropdown, name, label );
   });
-} // end buildDropdown
-
-/**
- * @description Build script to inject
- * @returns {string} function code to execute
- */
-function buildScript (wpClasses) {
-
-  /**
-   * @description 
-   * @param {string} name function name
-   * @param {string} code function code
-   * @returns {string} formated code
-   */
-  function formatCode ( name, code, sep = ',\n\n' ) {
-    /* format a code as
-      `${name}: ${code.toString()}`
-    */
-    return `${name}: ${code.toString()}${sep}`
-  }
-
-
-  debugger;
-  console.log('wpClasses ' + wpClasses);
 
   let scriptString = '';
-
-  // build script
-  data.forEach( ({name, label, code}) => { 
-      scriptString += formatCode ( name, code );
+  tasks.forEach( ({name, label, code}) => { 
+    scriptString += formatCode ( name, code );
   });
 
-  return `/* RDM Worpress Information object */
+  const script = 
+  `/* RDM Worpress Information object */
 
-  function logWPEditLink ( classNameString, beginsWithStrings ) {
-      let arrBeginsWith = [];
-      
-      if ( !classNameString || !beginsWithStrings ) return false;
-
-      if ( Array.isArray(beginsWithStrings) ) {
-      arrBeginsWith = beginsWithStrings;
-      } else if ( typeof(beginsWithStrings) === 'string' ) {
-      arrBeginsWith[0] = beginsWithStrings;
-      } else 
-      return false;
-
-      for (const strBegin of arrBeginsWith) {
-      if ( classNameString.includes( strBegin ) ) {
-          let idString = findId ( classNameString, strBegin );
-
-          console.log( "  " + window.location.origin + "/wp-admin/post.php?post=" + idString + "&action=edit" );
-          return true;
-      }        
-      }
-
-      return false;
+  function logWPEditLink ( classNameString, beginsWithString ) {
+    if ( classNameString.includes( beginsWithString ) ) {
+      showLink( findId ( classNameString, beginsWithString ) );
+    }
   }
 
   function findId ( string, matchString ) {
-      let pos = matchString.length - string.length;
-      return string.slice( pos );
+    let pos = matchString.length - string.length;
+    return string.slice( pos );
+  }
+  
+  function showLink ( pageId ) {
+    console.log( "RDM - Page edit url: ");
+    console.log( "  " + window.location.origin + "/wp-admin/post.php?post=" + pageId + "&action=edit" );
   }
 
   let rdm = (function() {
-    debugger;
-    console.log('wpClasses ' + wpClasses);
-    
+
     let domain = window.location.origin;
-    let wpClasses = ${wpClasses};
 
     return {
-    ${scriptString}
+      ${scriptString}
     }; // end return
   })();   
   `;
-} // end buildScript
+
+  getCurrentTabUrl( (tab) => {  // this closure is the callback
+  
+    dropdown.addEventListener('change', function() {
+
+      chrome.tabs.executeScript({ // this execute selected code on tab
+        code: `{  
+          ${script}
+  
+          rdm.${ this.value }();
+        }`
+      });
+  
+
+    }); // end addEventListener('change')
+
+    /* NOTE The following block of code does not work. It does not have any collection with previous .executeScript calls
+    let script = 'var hello = function() { alert("Hello World!!"); } ';
+    chrome.tabs.executeScript({ // this execute some code on tab
+      code: `{ 
+        console.trace('RDM WpInfo ext: trace - chrome.tabs.executeScript 2'); 
+        let sElement = document.createElement('script');
+        sElement.setAttribute('id', 'WpInfoExt');
+        sElement.innerHTML = '${script}';
+        document.querySelector('head').appendChild(sElement);
+      }`
+    });
+
+    chrome.tabs.executeScript({ // this execute some code on tab
+      code: `{ 
+        console.trace('RDM WpInfo ext: trace - chrome.tabs.executeScript 3 - calling an injected function'); 
+        window.hello();
+
+      }`
+    });
+    */
+    
+  }); // end callback
+}); // end addEventListener
