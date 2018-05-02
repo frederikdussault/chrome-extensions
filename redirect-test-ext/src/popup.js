30e025ca48698a65420c1af5f88e34eb80d36d9f
Frederik Dussault <fdussault7@gmail.com>
2018-05-02 14:50:17 -0400

    final redirect test extension

--- redirect-test-ext/src/popup.js
+++ redirect-test-ext/src/popup.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* https://www.texture.ca/wp-admin/tools.php?page=redirection.php */
+
 /**
  * Get the current URL.
  *
@@ -28,373 +30,187 @@ function getCurrentTabUrl(callback) {
 
 
 
-// This extension inject a script in the current tab. Script from which we will fetch information from the DOM. 
+// This extension inject a script in the current tab. Script from which we will fetch information from the DOM.
 // The user can select from the dropdown which information he wants for the
 // current.
 document.addEventListener('DOMContentLoaded', () => {
 
   /**
-   * @description format a code as js litteral notation: `${name}: ${code.toString()}`
-   * @param {string} name function name
-   * @param {string} code function code
-   * @returns {string} formated code
-   */
-  function formatCode ( name, code, sep = ',\n\n' ) {
-    return `${name}: ${code.toString()}${sep}`
-  }
-  
-  
-  /**
    * @description build html select field options based on data object
-   * @param {string} elementID
-   * @param {string} value
-   * @param {string} label
+   * @param {HTMLElement} elementID
+   * @param {string} name
+   * @param {Array} arr
    */
-  function appendToDropdown ( elemID, value, label ) {
+  function appendRadioButtons ( container, index, name) {
     // create an option: <option value="${value}">${label}</option>
-    // add option to elemID
-    let optionEl = document.createElement('option');
-        optionEl.setAttribute('value', value);
-        optionEl.innerHTML = label;
-    elemID.appendChild(optionEl);
+    // add option to container
+
+    //debugger;
+
+    let elId = container.id;
+    let radioId = elId + '_' + index;
+
+    let div = document.createElement('div');
+
+    let radio = document.createElement('input');
+        radio.setAttribute('type', 'radio');
+        radio.setAttribute('name', elId);
+        radio.setAttribute('id', radioId);
+        radio.setAttribute('class', 'radio');
+        radio.setAttribute('value', index);
+    div.appendChild(radio);
+
+    let label = document.createElement('label');
+        label.setAttribute('for', radioId);
+        label.innerHTML = name;
+    div.appendChild(label);
+
+    container.appendChild(div);
   }
 
 
-  /* ------------------------ */
-  const data = JSON.stringify(redirectList);
-
-  let tasks = [
-    {
-      'name': 'allTasks',
-      'label': 'All the options',
-      'code': function() {
-        //TODO: Add calls to all tasks defined
-
-        setTimeout(function() { alert("All the options clicked!"); }, 1);
-      }
-    },
-    {
-      'name': 'execRedirectionTest',
-      'label': 'Test the redirections',
-      'code': function() {
-        let compareAssert = (a, b) => {
-          if (a.assert < b.assert) return -1;
-          if (a.assert > b.assert) return  1;
-      
-          return 0;            
-        };
-
-        let compareStatus = (a, b) => {
-          if (a.resStatus < b.resStatus) return -1;
-          if (a.resStatus > b.resStatus) return  1;
-
-          return 0;            
-        };
-
-        let compareRequested = (a, b) => {
-          if (a.reqUrl < b.reqUrl) return -1;
-          if (a.reqUrl > b.reqUrl) return  1;
-      
-          return 0;
-        };
-
-        let compareStatusThenRequested = (a, b) => {
-          let res = compareStatus(a, b);
-
-          // status is equal, compare the next level: Requested url
-          if ( res === 0 )  {
-            return compareRequested(a, b);
-          }
-      
-          return res;
-        };
-
-        let compareAssert_Status_Requested = (a, b) => {
-          let res = compareStatus(a, b);
-
-          // status is equal, compare the next level: Requested url
-          if ( res === 0 )  {
-            return compareRequested(a, b);
-          }
-      
-          // status is equal, compare the next level: Requested url
-          if ( res === 0 )  {
-            return compareAssert(a, b);
-          }
-      
-          return res;
-        };
-
-        let compareRequestedThenStatus = (a, b) => {
-          let res = compareRequested(a, b);
-
-          // status is equal, compare the next level: Requested url
-          if ( res === 0 )  {
-            return compareStatus(a, b);
-          }
-      
-          return res;
-        };
-
-        console.log(`Number of record to process: ${redirectList.length}.`);
-        
-        let promises = [];
-
-        redirectList.forEach( function(element) {
-            promises.push(
-                // return a promise
-        
-                fetch(element.reqUrl)
-                    .then((response) => {
-                        let resUrl = (new URL(response.url)).pathname;
-        
-                        let assert = false;
-                        if (404 == element.expStatus && element.expStatus == response.status) {
-                            assert = true;
-                        }
-                        else if (element.expStatus == response.status && element.expUrl == resUrl) {
-                            assert = true;
-                        }
-                    
-                        element.resUrl = resUrl;
-                        element.resStatus = response.status;
-                        element.assert = (assert) ? 'OK' : 'FAIL';
-                    
-                        //console.log(response.url);
-                    }).catch ((error) => {
-                        console.log('Error: ', error);
-                    })
-        
-            );
-        });
-        
-        Promise.all(promises).then(() => {
-          redirectList.sort(compareStatusThenRequested);
-          console.table(redirectList, ['assert','reqUrl','expUrl','expStatus','resUrl','resStatus','reqNotes']);
-        });
-      }
-    },
-    {
-      'name': 'expandRedirectRules',
-      'label': 'Expand all rules',
-      'code': function() {
-        const n_redirectEditLinks = n_list.querySelectorAll('a.red-ajax');
-
-        n_redirectEditLinks.forEach( a => a.click() );
-      }
-    },
-    {
-      'name': 'displayRedirectionRulesNames',
-      'label': 'Get and list redirection rules names',
-      'code': function() {
-        developAllRulesOnRedirectionAdminPage( gatherData, displayNames );
-      },
-    },
-    {
-      'name': 'displayRedirectionRulesAsTabulatedCSV',
-      'label': 'Get and list redirection rules as csv',
-      'code': function() {
-        developAllRulesOnRedirectionAdminPage( gatherData, displayDataAsTabulatedCSV );
-      },
-    },
-    {
-      'name': 'displayRedirectionRulesAsJson',
-      'label': 'Get redirection rules as json',
-      'code': function() {
-        //TODO: Add code
-        setTimeout(function() { alert("displayRedirectionRulesAsJson clicked!"); }, 1);
-      },
+  /**
+   * @description build the task dropbox
+   * @param {HTMLElement} dropdown
+   */
+  const buildDropdown = (dropdown, taskArray) => {
+    /**
+     * @description build html select field options based on data object
+     * @param {string} elementID
+     * @param {string} value
+     * @param {string} label
+     */
+    function appendToDropdown ( elemID, label, value ) {
+      // create an option: <option value="${value}">${label}</option>
+      // add option to elemID
+      let option = document.createElement('option');
+          option.setAttribute('value', value);
+          option.innerHTML = label;
+      elemID.appendChild(option);
     }
-  ]; // end data
 
-  let dropdown = document.getElementById('dropdown');
-  tasks.forEach( ({name, label, code}) => appendToDropdown ( dropdown, name, label ) );
+    taskArray.forEach( ({name, label, code}, index) => appendToDropdown ( dropdown, label, index ) );
+  };
 
-  let scriptString = '';
-  tasks.forEach( ({name, label, code}) => scriptString += formatCode ( name, code ) );
-
-  const script = `
-  /* RDM Webproducer's Redirection Test tool */
-  
-  let rdm = (function() {
-    return {
-      ${scriptString}
-    }; // end return
-    
-  })(); // end rdm
-
-
-  /** ===========================================================
-
-   * util functions 
-  
-     ===========================================================*/
-  
-  /**
-   * gatherData
-   * @returns <Array of object> data
-   */
-  let gatherData = () => {
-
-      if ( blockedStatus ) {
-          setTimeout(function() { alert("A long process is opening the redirection information fields. Please patient a bit."); }, 1);
-          console.log("A long process is opening the redirection information fields. Please patient a bit.");
-          return [];
-      }
-
-      let data = [],
-          n_redirectNodes = n_list.querySelectorAll('table.edit'),
-          nbRules = n_redirectNodes.length;
-
-      if ( nbRules <= 0 ) {
-          console.error("No redirection found.  Data gathering stopped.");
-      } else {
-
-          const timedelay =  5;
-          let totaldelay = nbRules * timedelay;
-
-          totaldelay = (totaldelay < 1000) ? 1000 : totaldelay;  // too fast and table.edit links are not generated yet.
-
-          console.log("gathering rule information... will take " + totaldelay / 1000 + " seconds");
-          window.setTimeout(function() { 
-              console.log("Rule information gathering is completed."); 
-          }, totaldelay);
-
-          n_redirectNodes.forEach( (n_rule) => {
-              //let n_rule = this;
-
-              if ( n_rule.length <= 0 ) {
-                  console.log("No rules found.  Data gathering stopped.");
-                  return;
-              }
-
-              // check if input[name="old"], input[name="regex"] and input[name="target"] are present
-              if ( 
-                  n_rule.querySelector('input[name="old"]').length <= 0 ||
-                  n_rule.querySelector('input[name="regex"]').length <= 0 ||
-                  n_rule.querySelector('input[name="target"]').length <= 0
-              ) {
-                  console.log("No information found.  Data gathering stopped.");
-                  return;
-              }
-
-              let item = {};
-                  item.rule    = n_rule.querySelector('input[name="old"]').value;
-                  item.isRegex = n_rule.querySelector('input[name="regex"]').checked;
-                  item.target  = n_rule.querySelector('input[name="target"]').value;
-
-              data.push(item);
-          });
-      }
-
-      return data;
-  }
 
   /**
-   * displayNames
-   * @param <Array of object> arrayOfRules
-   * @returns none
+   * @description build the radio selectors
+   * @param {HTMLElement} testselector
+   * @param {Collection of test blocks}
+   * @returns {NodeList} radio selectors
    */
-  let displayNames = ( arrayOfRules ) => {
+  const buildRadioSelector = (testselector, testblocks) => {
+    //debugger;
 
-      console.log("Number of records loaded " + arrayOfRules.length );
+    for ([name, value] of Object.entries(testblocks) ) {
+      console.log( name, value  );
 
-      arrayOfRules.forEach( (r) =>  
-          console.log( r.rule )
-      );
+      appendRadioButtons (
+        testselector,
+        name, // eg 'DNR' or 'taxtest'  usefull to access the data
+        value.label )
+    }
 
+    return testselector.querySelectorAll('.radio');
   };
 
   /**
-   * displayDatatableAsJson
-   * @param <Array of object> arrayOfRules
-   * @returns none
+   * @description create an array of selected testblocks
+   * @param {HTMLElements} inputSelectors
+   * @param {Collection of test blocks} testblocks
+   * @returns Array of selected testblocks keys
    */
-  let displayDatatableAsJson = ( arrayOfRules ) => {
+  const getSelectedRadio = (inputSelectors) => {
 
-      console.log("Number of records loaded " + arrayOfRules.length );
-      
-    // print data array in console, formated as a table
-    //console.table( arrayOfRules );
+    //debugger;
 
-    // print data array in console, formated as a JSON
-    console.log( JSON.stringify(arrayOfRules) );
-  };
+    let selectedRadio = [];
 
-  /**
-  * displayDataAsTabulatedCSV
-  * @param <Array of object> arrayOfRules
-  * @returns none
-  */
-  let displayDataAsTabulatedCSV = ( arrayOfRules ) => {
-    
-    console.log("Number of records loaded " + arrayOfRules.length );
-
-    const lineArray = arrayOfRules.map( item => {
-        return '"' + 
-               item.rule + "\t" + 
-               (item.isRegex == true ? 'Y' : 'N') + "\t" + 
-               item.target + '"';
+    inputSelectors.forEach( (input) => {
+      if ( input.checked  ) {
+        selectedRadio.push( input.value );
+      }
     });
-    
-    var csvContent = lineArray.join( "\\n" );
 
-    // print data array in console, formated as a JSON
-    console.log( csvContent );
+    return selectedRadio;
   };
 
-
-
-  /**
-   * developAllRulesOnRedirectionAdminPage
-   * @returns none
-   */
-  let developAllRulesOnRedirectionAdminPage = ( cbGetData, cbDisplayData ) => {
-      // click all redirection edit link present on the page
-      const nbLinks = n_redirectEditLinks.length;
-      const timedelay =  50;
-      const totaldelay = nbLinks * timedelay;
-
-      blockedStatus = true;
-
-      console.log("Clicking on Redirection Edit links. There is " + nbLinks + " links to click. It will take " + (totaldelay / 1000) + " seconds");
-      
-      window.setTimeout(function() {
-          blockedStatus = false;
-          alert("Redirection Edit information are opened.");
-
-          // process and display the data if callbacks are provided
-          if ( typeof(cbDisplayData) === "function" && typeof(cbGetData) === "function") {
-              cbDisplayData( cbGetData() );
-          }
-      }, totaldelay);
-      
-      n_redirectEditLinks.forEach( a => a.click() );
-  }; //developAllRulesOnRedirectionAdminPage
-  `;
+  /* --------------------------------------------------------------------- */
 
   getCurrentTabUrl( (tab) => {  // this closure is the callback
-  
+
+    let dropdown = document.getElementById('dropdown');
+    let testselector = document.getElementById('testselector');
+
+    // inject tasks functions
+    chrome.tabs.executeScript(null,
+      {file:"tasks/alltasks.js"});
+    chrome.tabs.executeScript(null,
+      {file:"tasks/redirectiontasks.js"});
+    chrome.tabs.executeScript(null,
+      {file:"tasks/test-series.js"});
+
+    let tasks = [
+      {
+        'name': 'expandRedirectRules',
+        'label': 'Expand all rules',
+        'code': function() {
+          chrome.tabs.executeScript(null, {code:"expandRedirectRules()"});
+        }
+      },
+      {
+        'name': 'displayRedirectionRulesNames',
+        'label': 'Get and list redirection rules names',
+        'code': function() {
+          chrome.tabs.executeScript(null, {code:"displayRedirectionRulesNames()"});
+        }
+      },
+      {
+        'name': 'displayRedirectionRulesAsTabulatedCSV',
+        'label': 'Get and list redirection rules as csv',
+        'code': function() {
+          chrome.tabs.executeScript({
+            code: "displayRedirectionRulesAsTabulatedCSV();"
+          });
+        }
+      },
+      {
+        'name': 'displayRedirectionRulesAsJson',
+        'label': 'Get redirection rules as json',
+        'code': function() {
+          chrome.tabs.executeScript({
+            code: "displayRedirectionRulesAsJson();"
+          });
+        }
+      },
+      {
+        'name': 'execRedirectionTest',
+        'label': 'Test the redirections',
+        'code': function() {
+          chrome.tabs.executeScript(null,
+            {code:"execRedirectionTest( ['DNR'] );"}
+          );
+
+        } // end code
+
+      },
+    ]; // end tasks
+
+    buildDropdown(dropdown, tasks);
     dropdown.addEventListener('change', function() {
 
-      chrome.tabs.executeScript({ // this execute selected code on tab
-        code: `{ 
+      //debugger;
 
-          const n_list = document.querySelector('#the-list');
+      tasks[ this.value ].code(); // this execute selected code on tab
 
-          let blockedStatus = false;
-          let domain = window.location.origin;
-          let redirectList = ${data}; 
+    });
 
-          //console.log("Number of records loaded " + redirectList.length);
+    let radioSelectors = buildRadioSelector(testselector, testblocks);
+    testselector.addEventListener('change', function() {
+      chrome.tabs.executeScript(null,
+        {code:"execRedirectionTest(" + JSON.stringify( getSelectedRadio(radioSelectors) ) + ");"}
+      );
+    });
 
-          ${script}
-
-          rdm.${ this.value }();
-        }`
-      });
-
-    }); // end addEventListener('change')
-    
-  }); // end callback
-}); // end addEventListener
+  }); // end getCurrentTabUrl callback
+}); // end document.addEventListener
